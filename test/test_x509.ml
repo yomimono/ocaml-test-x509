9fb50aeb9bffafbd025750ef09e5ea7989d88c4d
Mindy <meetup@yomimono.org>
2018-02-13 17:46:36 -0600

    make components

--- test/test_x509.ml
+++ test/test_x509.ml
@@ -1,6 +1,24 @@
-module type Certificate = [%import: (module X509)] [@@deriving crowbar]
+module Asn = struct
+  include Asn
+  let oid_base_to_crowbar =
+    (* OID bases are different depending on the leading integer.
+       if the first number is 0 or 1, the second number is 0..39 inclusive;
+       if the first number is 2, the second number is any nonnegative number. *)
+    let early_edition =
+      Crowbar.(map [choose [const 0; const 1]; range 40] Asn.OID.base)
+    in
+    let late_edition = Crowbar.(map [const 2; range max_int] Asn.OID.base) in
+    Crowbar.(choose [early_edition; late_edition])
+
+  let oid_to_crowbar = Crowbar.(map [oid_base_to_crowbar; list (range max_int)]
+                                  Asn.OID.(<||))
+end
+
+module X509 = struct
+  type component = [%import: X509.component] [@@deriving crowbar]
+end
 
 let () =
-   Crowbar.(add_test ~name:"t is derivable" [Certificate_to_crowbar.to_crowbar]
-(fun t -> check true))
+   Crowbar.(add_test ~name:"t is derivable" [X509.component_to_crowbar]
+              (fun t -> check true));
   ()
