c22a31ee78f3e052319095624ced45f1cb42f42e
Mindy <meetup@yomimono.org>
2018-02-15 15:25:39 -0600

    derive harder

--- test/test_x509.ml
+++ test/test_x509.ml
@@ -1,5 +1,7 @@
 module Asn = struct
   include Asn
+  let pp_oid = Asn.OID.pp
+  let equal_oid = Asn.OID.equal
   let oid_base_to_crowbar =
     (* OID bases are different depending on the leading integer.
        if the first number is 0 or 1, the second number is 0..39 inclusive;
@@ -16,6 +18,7 @@ end
 
 module Cstruct = struct
   include Cstruct
+  let pp fmt s = Format.fprintf fmt "%s" (Cstruct.to_string s)
   let to_crowbar = Crowbar.(map [bytes] Cstruct.of_string)
 end
 
@@ -36,71 +39,80 @@ module Ptime = struct
   include Ptime
 end
 
+module Z = struct
+  include Z
+  let to_crowbar = Crowbar.(map [int64] Z.of_int64)
+  let pp fmt z = Format.fprintf fmt "%s" @@ Z.to_string z
+end
+
+type hash = [%import: Nocrypto.Hash.hash] [@@deriving crowbar, show, eq]
+
 module Crowbar_X509 = struct
-  open X509
-  type key_type' = [%import: X509.key_type ] [@@deriving crowbar]
-  let key_type_to_crowbar = key_type'_to_crowbar
-  type host' = [%import: X509.host ] [@@deriving crowbar]
-  let host_to_crowbar = host'_to_crowbar
-  type component' = [%import: X509.component ] [@@deriving crowbar]
-  let component_to_crowbar = component'_to_crowbar
-  let distinguished_name_to_crowbar = Crowbar.list component_to_crowbar
+  type key_type = [%import: X509.key_type ] [@@deriving crowbar, show, eq]
+  type host = [%import: X509.host ] [@@deriving crowbar, show, eq]
+  type component = [%import: X509.component] [@@deriving crowbar, show, eq]
+  type distinguished_name = [%import: X509.distinguished_name ] [@@deriving crowbar, show, eq]
+
+  type public_key = X509.public_key
+  type private_key = X509.private_key
 
   (* keypairs are special -- always use the precomputed ones *)
   let public_key_to_crowbar : X509.public_key Crowbar.gen =
-    Crowbar.const X509.(`RSA (Nocrypto.Rsa.pub_of_priv Keys.priv))
+    Crowbar.const X509.(`RSA (Nocrypto.Rsa.pub_of_priv Keys.csr_priv))
   let private_key_to_crowbar : X509.private_key Crowbar.gen =
-    Crowbar.const X509.(`RSA Keys.priv)
+    Crowbar.const X509.(`RSA Keys.csr_priv)
+  let pp_public_key fmt _ = Format.fprintf fmt "%s" "the public key"
+  let pp_private_key fmt _ = Format.fprintf fmt "%s" "the private key"
+  let equal_public_key _ _ = true
+  let equal_private_key _ _ = true
 
   module Extension = struct
-    include X509.Extension
-    type key_usage' = [%import: X509.Extension.key_usage] [@@deriving crowbar]
-    let key_usage_to_crowbar = key_usage'_to_crowbar
-    type extended_key_usage' = [%import: X509.Extension.extended_key_usage]
-    [@@deriving crowbar]
-    let extended_key_usage_to_crowbar = extended_key_usage'_to_crowbar
-    type general_name' = [%import: X509.Extension.general_name] [@@deriving crowbar]
-    let general_name_to_crowbar = general_name'_to_crowbar
-    (* our authority_keys will always have None as their third element 
-       to (cowardly) avoid Z.to_crowbar *)
-    let authority_key_id_to_crowbar =
-        Crowbar.(map [option Cstruct.to_crowbar; list general_name_to_crowbar]
-		    (fun a b -> a, b, None))
-    type priv_key_usage_period' = [%import: X509.Extension.priv_key_usage_period]
-       [@@deriving crowbar]
-    let priv_key_usage_period_to_crowbar = priv_key_usage_period'_to_crowbar
-    type name_constraint' = [%import: X509.Extension.name_constraint] [@@deriving crowbar]
-    let name_constraint_to_crowbar = name_constraint'_to_crowbar
-    type policy' = [%import: X509.Extension.policy] [@@deriving crowbar]
-    let policy_to_crowbar = policy'_to_crowbar
-    type reason' = [%import: X509.Extension.reason] [@@deriving crowbar]
-    let reason_to_crowbar = reason'_to_crowbar
-    type reason_code' = [%import: X509.Extension.reason_code] [@@deriving crowbar]
-    let reason_code_to_crowbar = reason_code'_to_crowbar
-    type distribution_point_name' = [%import: X509.Extension.distribution_point_name] [@@deriving crowbar]
-    let distribution_point_name_to_crowbar = distribution_point_name'_to_crowbar
-    type distribution_point' = [%import: X509.Extension.distribution_point] [@@deriving crowbar]
-    let distribution_point_to_crowbar = distribution_point'_to_crowbar
-    type t' = [%import: X509.Extension.t] [@@deriving crowbar]
-    let to_crowbar = t'_to_crowbar
+    type key_usage = [%import: X509.Extension.key_usage] [@@deriving crowbar, show, eq]
+    type extended_key_usage = [%import: X509.Extension.extended_key_usage] [@@deriving crowbar, show, eq]
+    type general_name = [%import: X509.Extension.general_name] [@@deriving crowbar, show, eq]
+    type authority_key_id = [%import: X509.Extension.authority_key_id] [@@deriving crowbar, show, eq]
+    type priv_key_usage_period = [%import: X509.Extension.priv_key_usage_period]
+       [@@deriving crowbar, show, eq]
+    type name_constraint = [%import: X509.Extension.name_constraint] [@@deriving crowbar, show, eq]
+    type policy = [%import: X509.Extension.policy] [@@deriving crowbar, show, eq]
+    type reason = [%import: X509.Extension.reason] [@@deriving crowbar, show, eq]
+    type reason_code = [%import: X509.Extension.reason_code] [@@deriving crowbar, show, eq]
+    type distribution_point_name = [%import: X509.Extension.distribution_point_name] [@@deriving crowbar, show, eq]
+    type distribution_point = [%import: X509.Extension.distribution_point] [@@deriving crowbar, show, eq]
+    type t = [%import: X509.Extension.t] [@@deriving crowbar, show, eq]
   end
   module CA = struct
-    include X509.CA
-    type request_extensions' = [%import: X509.CA.request_extensions] [@@deriving crowbar]
-    let request_extensions_to_crowbar = request_extensions'_to_crowbar
-    type request_info' = [%import: X509.CA.request_info] [@@deriving crowbar]
-    let request_info_to_crowbar = request_info'_to_crowbar
+    type request_extensions = [%import: X509.CA.request_extensions] [@@deriving crowbar, show, eq]
+    type request_info = [%import: X509.CA.request_info] [@@deriving crowbar, show, eq]
     let signing_request_to_crowbar = Crowbar.(map [distinguished_name_to_crowbar;
                                                   list request_extensions_to_crowbar;
-                                                  private_key_to_crowbar] (fun dn extensions key ->
-        X509.CA.request dn ~extensions key))
+                                                  hash_to_crowbar;
+                                                  private_key_to_crowbar] (fun dn extensions digest key ->
+        X509.CA.request dn ~extensions ~digest key))
   end
 end
 
 let () =
+  let seed = Cstruct.of_string "yolocryptolol" in
   (* we need to tell nocrypto to use a constant seed *)
   let g = Nocrypto.Rng.generator in
-  g := Nocrypto.Rng.(create ~seed:(Cstruct.of_string "yolocrypto") (module Generators.Fortuna));
-   Crowbar.(add_test ~name:"csrs can be made" [Crowbar_X509.CA.signing_request_to_crowbar]
-              (fun t -> check true));
-  ()
+  g := Nocrypto.Rng.(create ~seed (module Generators.Fortuna));
+  (* something in our calls to signing_request lowers stability. *)
+  Crowbar.(add_test ~name:"same csr twice" [Crowbar_X509.distinguished_name_to_crowbar;
+                                              list Crowbar_X509.CA.request_extensions_to_crowbar;
+                                              hash_to_crowbar;
+                                              Crowbar_X509.private_key_to_crowbar] 
+	     (fun dn extensions digest key ->
+                 let csr1 = X509.CA.request dn ~extensions ~digest key in
+                 let csr2 = X509.CA.request dn ~extensions ~digest key in
+                 let pem1 = X509.Encoding.cs_of_signing_request csr1 in
+                 let pem2 = X509.Encoding.cs_of_signing_request csr2 in
+                 Crowbar.check_eq ~pp:Cstruct.hexdump_pp ~cmp:Cstruct.compare ~eq:Cstruct.equal pem1 pem2
+   (* this test has a stability that quickly recovers from 90sish, not clear to me why.*)
+
+  ));
+  Crowbar.(add_test ~name:"csr PEM roundtrip" [Crowbar_X509.CA.signing_request_to_crowbar] (fun csr -> let pem = X509.Encoding.Pem.Certificate_signing_request.to_pem_cstruct1 csr in
+Printf.printf "%s\n%!" (Cstruct.to_string pem);
+             let read_csr = X509.Encoding.Pem.Certificate_signing_request.of_pem_cstruct1 pem in
+             Crowbar.check_eq ~pp:Cstruct.hexdump_pp ~cmp:Cstruct.compare ~eq:Cstruct.equal pem @@ X509.Encoding.Pem.Certificate_signing_request.to_pem_cstruct1 read_csr
+))
