1c122e64f1e058c26017f89fe7fefd0ab9687ab4
Mindy <meetup@yomimono.org>
2018-04-16 20:17:44 -0500

    print the cert we test against

--- test/test_x509.ml
+++ test/test_x509.ml
@@ -98,6 +98,7 @@ how ridiculous they were about signing it"
              `Fail (`InvalidChain)
            in
            let is_failure = function | `Fail _ -> true | `Ok _ -> false in
+           Format.printf "%s\n" (X509.sexp_of_t signed_by_rando |> Sexplib.Sexp.to_string_hum);
            check @@ is_failure @@
              X509.Validation.verify_chain_of_trust ~anchors:[real_ca] [signed_by_rando]
           )
